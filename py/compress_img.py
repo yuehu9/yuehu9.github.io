96d72c195aa240f0ef34033887608c194bd19414
Yue Hu <yue.hu@vanderbilt.edu>
2019-08-18 14:50:34 -0500

    fixed bug

--- py/compress_img.py
+++
@@ -1,9 +0,0 @@
-
-from PIL import Image
-import glob
-
-for filename in glob.glob('../images/gallery/*.JPG'):
-    img = Image.open(filename)
-    m, n = img.size
-    img = img.resize((m//2, n//2), Image.ANTIALIAS)
-    img.save(filename, optimize=True, quality=85)
